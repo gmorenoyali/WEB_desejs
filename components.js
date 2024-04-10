545f9f1300afe860c26b22e22cb1919203ce4739
GiiO <giiomy5@gmail.com>
2024-04-09 20:21:37 -0500

    Cambios en el contenido

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 // Define los componentes HTML
 const headerComponent = `
     <header>
-        <h1>Mi Página Web</h1>
+        <h1>W3D</h1>
     </header>
 `;
 
@@ -25,20 +25,20 @@ const mainComponent = `
         <section>
             <h2>Nuestros Servicios</h2>
             <ul>
-                <li>Servicio 1</li>
-                <li>Servicio 2</li>
-                <li>Servicio 3</li>
+                <li>Diseño WEB</li>
+                <li>Video de Drones</li>
+                <li>Impresión 3d</li>
             </ul>
         </section>
         <section>
-            <h2>Contacto</h2>
-            <p>¡Estamos aquí para ayudarte! Contáctanos a través del formulario siguiente:</p>
+            <h2>¡Contactanos!</h2>
+            <p>¡Estamos aquí para ayudarte a hacer realidad tus ideas mas locas!</p>
             <form>
-                <label for="nombre">Nombre:</label>
+                <label for="nombre">Tu nombre:</label>
                 <input type="text" id="nombre" name="nombre" required>
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email" required>
-                <label for="mensaje">Mensaje:</label>
+                <label for="mensaje">Danos una breve descriocion de tu idea:</label>
                 <textarea id="mensaje" name="mensaje" required></textarea>
                 <button type="submit">Enviar</button>
             </form>
